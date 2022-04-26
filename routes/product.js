697612213a97617975ed33168b14721539fa038f
createvaluefor <2019143077@yonsei.ac.kr>
2022-04-26 23:35:08 +0900

    search query added

--- routes/product.js
+++ routes/product.js
@@ -6,25 +6,53 @@ const ProductColor = require("../models/product_color");
 const ProductDisplay = require('../models/product_display');
 const ProductImage = require('../models/product_images');
 const SubCategory = require('../models/sub_category');
+const { Op } = require("sequelize")
 
 
 // 상품 전체 불러오기
 router.get('/color', async (req, res, next) => {
     console.log(req.query);
+    const { categoryId, theme } = req.query;
+    console.log(categoryId, theme)
 
-    const data = await ProductColor.findAll({
+    const productSearchOption = {
+        model: Product,
 
-        include: [ProductDisplay, ProductImage, {
-            model: Product,
+        attributes: ['title', 'id', 'price', 'categoryId', 'detail'],
+        include: [SubCategory]
+    }
+    if (categoryId) {
+        productSearchOption.where = { categoryId }
+    }
+    const searchOptions = {
 
-            attributes: ['title', 'id', 'price', 'categoryId', 'detail'],
-            include: [SubCategory]
-        },
+        include: [ProductDisplay, ProductImage, productSearchOption,
             {
                 model: ProductColorSize,
                 attributes: ['id', 'size', 'count']
             }]
-    })
+    }
+
+    const whereOption = {}
+    if (theme === 'sale') {
+        whereOption.salePercent = { [Op.gt]: 0 }
+    }
+    if (theme === 'best') {
+        whereOption.saleCount = { [Op.gt]: 10 }
+    }
+    if (theme === 'new') {
+        whereOption.new = true
+    }
+
+    if (theme) {
+        searchOptions.where = whereOption
+    }
+
+
+
+
+
+    const data = await ProductColor.findAll(searchOptions)
 
     return res.json({
         success: true,
